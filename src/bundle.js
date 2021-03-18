42fb9db5f74c18fcd328e5ab12c4aedf18228d17
Lily Scott <me@suchipi.com>
2021-03-18 05:04:43 -0600

    Update docs to reflect this is a fork, change package.json to scoped, run prettier on all the js

--- src/bundle.js
+++ src/bundle.js
@@ -1,15 +1,17 @@
-var path = require('path');
-var browserify = require('browserify');
+var path = require("path");
+var browserify = require("browserify");
 
-var entry = './src/interface';
-var output = './examples/bundle.js';
+var entry = "./src/interface";
+var output = "./examples/bundle.js";
 
-var result = new Promise(function(res, rej) {
-    var bundle = browserify('interface', { standalone : 'Gameboy' });
+var result = new Promise(function (res, rej) {
+  var bundle = browserify("interface", { standalone: "Gameboy" });
 
-    bundle.bundle(function (err, buffer) {
-        if (err) { throw new Error(err); }
+  bundle.bundle(function (err, buffer) {
+    if (err) {
+      throw new Error(err);
+    }
 
-        res(buffer);
-    });
+    res(buffer);
+  });
 });
--- tests/blargg/runner.js
+++ tests/blargg/runner.js
@@ -1,17 +1,17 @@
-var Distilled = require('@latinfor/distilled');
+var Distilled = require("@latinfor/distilled");
 
 var tests = {
-    cpu: {
-        file: './roms/blargg_tests/cpu_instrs.gb',
-        duration: 4200
-    },
+  cpu: {
+    file: "./roms/blargg_tests/cpu_instrs.gb",
+    duration: 4200,
+  },
 
-    //other test ROMs here
+  //other test ROMs here
 };
 
 var regenerate = (function () {
-    var args = process.argv.slice(2);
-    return args.includes('generate');
-}());
+  var args = process.argv.slice(2);
+  return args.includes("generate");
+})();
 
-var results = require('')
+var results = require("");
