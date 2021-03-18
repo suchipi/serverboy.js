6c73aafcff6c0a0f14fc5da24e7b66246d99b4c3
Lily Scott <me@suchipi.com>
2021-03-18 05:27:36 -0600

    Make shutdownEmulation public

--- src/interface.js
+++ src/interface.js
@@ -16,24 +16,25 @@ const PRIVATE = "_" + process.hrtime().join(".");
 function Interface() {
   let _that = (this[PRIVATE] = {
     __proto__: Interface._prototype,
-    gameboy: null, //Core emulator
-    frames: 0, //Number of elapsed frames
-    pressed: new Array(Object.keys(KEYMAP).length), //Which keys are currently being held down
+    gameboy: null, // Core emulator
+    frames: 0, // Number of elapsed frames
+    pressed: new Array(Object.keys(KEYMAP).length), // Which keys are currently being held down
   });
   _that[PRIVATE] = _that;
 }
 
 Interface._prototype = {
-  //Check to make sure the gameboy object has been created.
+  // Check to make sure the gameboy object has been created.
   initialized: function () {
     let _that = this[PRIVATE];
     return typeof _that.gameboy === "object" && _that.gameboy != null;
   },
 
   /*
-   *Make sure that the emulator is "running" (note that this is different than play/pause)
-   *Think of it like turning the key in your ignition before you start driving your car
-   *TODO: does `true` mean it's running or does `true` mean it's stopped?
+   * Make sure that the emulator is "running" (note that this is different than play/pause)
+   * Think of it like turning the key in your ignition before you start driving your car
+   *
+   * Returns true or false; true means running, false means stopped.
    */
   running: function () {
     let _that = this[PRIVATE];
@@ -54,15 +55,6 @@ Interface._prototype = {
       _that.gameboy.JoyPadEvent(keycode, down);
     }
   },
-
-  //Stop emulator, reset relevant variables
-  shutdownEmulation: function () {
-    let _that = this[PRIVATE];
-    if (_that.initialized() && _that.running()) {
-      _that.gameboy.stopEmulator |= 2;
-      _that.frames = 0; //Reset internal variables
-    }
-  },
 };
 
 Interface.prototype = {
@@ -75,15 +67,9 @@ Interface.prototype = {
    * TODO: better documentation on what ROMs are and how they should be formatted.
    */
   loadRom: function (ROM) {
-    let that = this;
     let _that = this[PRIVATE];
 
-    // if (!_that.initialized()) {
-    //     return false;
-    // }
-
-    //TODO: autosave last state?
-    _that.shutdownEmulation(); //Will shut down emulator if it's still running.
+    this.shutdownEmulation(); // Will shut down emulator if it's still running.
 
     _that.gameboy = new GameBoyCore(ROM);
 
@@ -176,22 +162,24 @@ Interface.prototype = {
   /*
    * Gets a block of memory, you can specify a start and an end if you want
    * - this is an expensive operation and should be called sparingly
-   *
-   * TODO perf test ``slice`` vs a ``for`` loop
    */
-  getMemory: function (start, end) {
+  getMemory: function () {
     let _that = this[PRIVATE];
-    start = Math.max(start, 0);
-    end = Math.min(end, _that.gameboy.memory.length);
-
     return _that.gameboy.memory;
-    //return _that.gameboy.memory.slice(start, end); //Why are you doing this functionally instead of just returning the entire object?
-    //Is it for speed?
   },
 
   getAudio: function () {
     let _that = this[PRIVATE];
     return _that.gameboy.audioBuffer;
+  },
+
+  // Stop emulator, reset relevant variables
+  shutdownEmulation: function () {
+    let _that = this[PRIVATE];
+    if (_that.initialized() && _that.running()) {
+      _that.gameboy.stopEmulator |= 2;
+      _that.frames = 0; // Reset internal variables
+    }
   },
 };
 
